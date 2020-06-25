d4fd048e7292c8665a2a63e20f27bac4654f7f38
Anna Goncharenko <agoncharenko@fb.com>
2020-06-25 12:17:43 -0700

    clicking on movie opens new activity

---
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -0,0 +1,24 @@
+package com.example.flixster;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.flixster.models.Movie;
+
+import org.parceler.Parcels;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+    Movie movie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+        // //unwrap movie passed in from intent
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
+    }
+}
--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,8 +15,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CenterInside;
+import com.example.flixster.MovieDetailsActivity;
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -58,7 +62,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         return movies.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView tvTitle;
         TextView tvOverview;
@@ -69,6 +73,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            itemView.setOnClickListener(this);
         }
 
         // Populate each views with movie properties
@@ -77,12 +82,33 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvOverview.setText(movie.getOverview());
             // Render poster image into view by landscape or portrait mode
             // landscape = backdrop, portrait = poster
-            String imageURL = (context.getResources().getConfiguration().orientation
-                    == Configuration.ORIENTATION_LANDSCAPE) ? movie.getBackdropPath() : movie.getPosterPath();
+            String imageURL;
+            int placeholder;
+            if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                imageURL = movie.getBackdropPath();
+                placeholder = R.drawable.backdrop_placeholder;
+            } else {
+                imageURL = movie.getPosterPath();
+                placeholder = R.drawable.movie_placeholder;
+            }
             int radius = 30; // corner radius
             int margin = 5;
-            Glide.with(context).load(imageURL)
+            Glide.with(context).load(imageURL).placeholder(placeholder)
                     .transform(new CenterInside(), new RoundedCornersTransformation(radius, margin)).into(ivPoster);
+        }
+
+        // show movie details when user clicks on a row
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            // if valid position, get movie at position, create new intent for new activity,
+            // serialize movie with parceler, and show the new activity
+            if(position != RecyclerView.NO_POSITION) {
+                Movie movie = movies.get(position);
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                intent.putExtra(Movie.class.getSimpleName(), Parcels.wrap(movie));
+                context.startActivity(intent);
+            }
         }
     }
 }
